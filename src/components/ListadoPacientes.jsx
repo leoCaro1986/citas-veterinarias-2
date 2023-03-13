d478f2f5610b6813e439f6e6dfc047262036306c
Leonardo <leocv0205@gmail.com>
2023-03-12 21:35:07 -0500

    Se crea iteración en el listado de pacientes y se genera id para que cada elemento sea unico

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,9 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = () => {
+const ListadoPacientes = ({pacientes}) => {
+
+
+
   return (
     <div className="md:w-1/2 lg:w-2/5 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
@@ -8,12 +11,12 @@ const ListadoPacientes = () => {
         Administra tus {""}
         <span className="text-indigo-600 font-bold">Pacientes y citas</span>
       </p>
-
-      <Paciente/>
-      <Paciente/>
-      <Paciente/>
-      <Paciente/>
-      <Paciente/>
+      {pacientes.map(paciente =>(
+          <Paciente
+            key={paciente.id}
+            paciente={paciente}
+          />
+        ))}
       
     </div>
   )
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,28 +1,28 @@
 
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+
+    const {nombre, propietario, email, fecha, sintomas} = paciente
+
   return (
     <div className="mx-3 my-10 bg-white shadow-md px-5 py-10 rounded-lg"> 
         <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
-            <span className="font-normal normal-case">Hook</span>
+            <span className="font-normal normal-case">{nombre}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""}
-            <span className="font-normal normal-case">Leonardo</span>
+            <span className="font-normal normal-case">{propietario}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""}
-            <span className="font-normal normal-case">@gmail.com</span>
+            <span className="font-normal normal-case">{email}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de Alta: {""}
-            <span className="font-normal normal-case">Diciembre</span>
+            <span className="font-normal normal-case">{fecha}</span>
         </p>
 
         <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {""}
-            <span className="font-normal normal-case">Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Recusandae adipisci impedit, distinctio 
-            nisi neque hic. Excepturi sequi, quibusdam, magni nulla nisi rem maiores 
-            ad reprehenderit dignissimos aut doloribus consequatur. Eius?</span>
+            <span className="font-normal normal-case">{sintomas}</span>
         </p>
     </div>
   )
