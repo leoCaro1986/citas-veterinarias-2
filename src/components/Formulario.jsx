e23e28877210200f5c851d111099e31538a1eca0
Leonardo Caro <leonardo.caro@sofka.local>
2023-03-17 19:23:17 -0500

    Se actualiza formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,9 @@
-
+import { useState, useEffect } from 'react';
 
 const Formulario = () => {
+  const [nombre, setNombre] = useState("Leo");
+
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">
